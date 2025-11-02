e769d7ae032ea3240dcd2e1fb1d1f893a1387853
llmnd <papendiaye511@gmail@example.com>
2025-11-02 18:00:56 +0000

    b4

--- src/theme.ts
+++ src/theme.ts
@@ -2,8 +2,8 @@ import { extendTheme } from '@chakra-ui/react'
 
 const theme = extendTheme({
   fonts: {
-    heading: `'Times New Roman', serif`,
-    body: `'Times New Roman', serif`,
+    heading: `'Roboto', sans-serif`,
+    body: `'Roboto', sans-serif`,
   },
   colors: {
     brand: {
