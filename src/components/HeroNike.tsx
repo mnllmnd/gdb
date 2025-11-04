13fd09b1109770080625009acd1425a219dea6dd
llmnd <papendiaye511@gmail@example.com>
2025-11-04 21:10:12 +0000

    b6

--- src/components/HeroNike.tsx
+++ src/components/HeroNike.tsx
@@ -6,7 +6,7 @@ const HeroNike: React.FC = () => {
 
   const images: string[] = [
     'https://i.pinimg.com/1200x/d6/b9/c2/d6b9c24dff769465c3f373f9bf5db15e.jpg',
-    'https://res.cloudinary.com/dcs9vkwe0/image/upload/v1761999319/gestion_de_boutique/mghjkzerwo2spwmobzaf.jpg',
+    'https://i.pinimg.com/1200x/12/0e/8c/120e8c840d0cd85f6757c62a5366c86d.jpg',
     'https://i.pinimg.com/1200x/0e/c5/56/0ec556681ae382c44ee5926fc752e72c.jpg',
   ]
 
