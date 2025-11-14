9cf13bcbafdae20aea9c4fadb4a193638816716f
llmnd <papendiaye511@gmail@example.com>
2025-11-14 19:53:01 +0000

    b12

--- src/components/HeroNike.tsx
+++ src/components/HeroNike.tsx
@@ -5,7 +5,7 @@ const HeroNike: React.FC = () => {
   const overlay = useColorModeValue('rgba(255,255,255,0.02)', 'rgba(0,0,0,0.6)')
 
   const images: string[] = [
-    'https://i.pinimg.com/736x/20/7d/b4/207db42131fce117f0b32a7381ff7298.jpg',
+    'https://i.pinimg.com/1200x/2f/81/1f/2f811feaa1c0553f2f924c623f0d39dc.jpg',
     'https://i.pinimg.com/1200x/f4/88/35/f488355a53b9431cf31a3f8492a77031.jpg',
     'https://i.pinimg.com/736x/cd/f3/5d/cdf35d0ef6b1e520acc76abcdfe728aa.jpg',
   ]
